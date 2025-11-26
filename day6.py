84a5fd05accf193f6ae1466434be680cd7ae4a3d
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2025-11-26 11:51:50 +0530

    py

--- day6.py
+++ day6.py
@@ -3,3 +3,4 @@ name = ["muskan", "prachi", "riya"]
 print(type(name))
 name.append("kajal")
 print(name)
+ 
---
+++ day7.py
@@ -0,0 +1,5 @@
+num=5
+for j in range(1,num+1):
+    for i in range(1,j+1):
+        print(i,end=' ')
+    print()    
