a197f71d8809e7efc4a866474878fa2b06baf305
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2025-12-11 00:00:34 +0530

    ff

--- day15.py
+++ day15.py
@@ -1,3 +1,14 @@
-def even (n):
-    return n%2 ==0
-print(even(10))
+#vote
+age = int(input("enter your age:"))
+if age >= 18:
+    print("vote")
+else :
+    print("cant vote")  
+
+
+#leap year
+year = int(input("enter a year:"))
+if (year % 4 ==0 and year % 100 != 0) or (year % 400 == 0):
+    print("leap year")
+else:
+    print("not a leap year")     
