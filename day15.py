29719244207c6abd6f5abc00ce09bab251d4d055
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2025-12-11 00:21:29 +0530

    ff

--- day15.py
+++ day15.py
@@ -1,14 +1,24 @@
 #vote
-age = int(input("enter your age:"))
+'''age = int(input("enter your age:"))
 if age >= 18:
     print("vote")
 else :
-    print("cant vote")  
+    print("cant vote") ''' 
 
 
 #leap year
-year = int(input("enter a year:"))
+'''year = int(input("enter a year:"))
 if (year % 4 ==0 and year % 100 != 0) or (year % 400 == 0):
     print("leap year")
 else:
-    print("not a leap year")     
+    print("not a leap year") '''
+
+
+# negetive , positive and zero
+num = float(input("enter a num:"))
+if num > 0:
+    print("positive")
+elif num < 0:
+    print("negetive")
+else:
+    print("zero")                
