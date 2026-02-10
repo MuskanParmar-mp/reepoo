bd78857aaa700e4d9be7e7608a17481fdc7131f5
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-10 23:51:08 +0530

    list

--- list.py
+++ list.py
@@ -18,3 +18,11 @@ thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
+
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+
+thistuple = ("apple")
+print(type(thistuple))
